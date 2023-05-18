109991ab439597c744dd7dfc1644e78c3ece3254
covarom <huudoan.qna@gmail.com>
2023-05-18 18:26:51 +0700

    add new lib

--- libs/homepage/src/lib/homepage/homepage.component.ts
+++ libs/homepage/src/lib/homepage/homepage.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-
+import {DestroyService} from 'libs/shared/services/destroy.service';
 @Component({
   selector: 'exe-project-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
+  providers: [DestroyService],
 })
 export class HomepageComponent {}
