d89988bfa7e171a4ee8335d8cc13b9bbf8030ceb
hieunmtse15-772 <nguyenminhtrunghieu10@gmail.com>
2023-05-18 21:31:42 +0700

    commit user page

--- libs/learning/src/lib/header/header.component.ts
+++ libs/learning/src/lib/header/header.component.ts
@@ -1,8 +1,12 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, NgModule, ViewEncapsulation} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'exe-project-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+}
