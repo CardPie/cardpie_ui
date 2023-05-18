fa6c56814682e4b35e837584bcab18c4b6dd3e2d
covarom <huudoan.qna@gmail.com>
2023-05-18 14:59:09 +0700

    learning page structure

--- libs/learning/src/lib/learning.module.ts
+++ libs/learning/src/lib/learning.module.ts
@@ -2,12 +2,20 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {learningRoutes} from './lib.routes';
-
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   imports: [
     CommonModule,
+    MatToolbarModule,
+    MatIconModule,
     RouterModule.forChild(learningRoutes),
     RouterModule.forChild(learningRoutes),
   ],
+  declarations: [HeaderComponent, FooterComponent, MainPageComponent],
+  exports: [HeaderComponent, FooterComponent, MainPageComponent],
 })
 export class LearningModule {}
--- libs/learning/src/lib/lib.routes.ts
+++ libs/learning/src/lib/lib.routes.ts
@@ -1,5 +1,6 @@
 import {Route} from '@angular/router';
+import {MainPageComponent} from './main-page/main-page.component';
 
 export const learningRoutes: Route[] = [
-  /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+  {path: '', pathMatch: 'full', component: MainPageComponent},
 ];
