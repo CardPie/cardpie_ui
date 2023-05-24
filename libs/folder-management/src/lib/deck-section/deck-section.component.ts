8c7ed19067e689cbf4ef0cacf4e1a67c4c337d35
covarom <huudoan.qna@gmail.com>
2023-05-25 00:14:54 +0700

    add deck section

---
+++ libs/folder-management/src/lib/deck-section/deck-section.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'exe-project-deck-section',
+  templateUrl: './deck-section.component.html',
+  styleUrls: ['./deck-section.component.scss'],
+})
+export class DeckSectionComponent {}
---
+++ libs/folder-management/src/lib/memorize-section/memorize-section.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'exe-project-memorize-section',
+  templateUrl: './memorize-section.component.html',
+  styleUrls: ['./memorize-section.component.scss'],
+})
+export class MemorizeSectionComponent {}
