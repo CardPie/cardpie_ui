d8901fb93ba0852ff156fd21e3066999e540e138
Truong Giang <giangntse150747@gmail.com>
2023-06-05 04:01:00 +0700

    add ui for register page

--- libs/auth/register/src/lib/auth-register.module.ts
+++ libs/auth/register/src/lib/auth-register.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {authRegisterRoutes} from './lib.routes';
 import {SignUpFormComponent} from './sign-up-form/sign-up-form.component';
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   imports: [
     CommonModule,
+    ToastrModule,
     RouterModule.forChild(authRegisterRoutes),
     RouterModule.forChild(authRegisterRoutes),
   ],
