b1ede55b14e7434d055fe9cc3ed5f10a152e770c
Truong Giang <giangntse150747@gmail.com>
2023-06-05 23:17:35 +0700

    revert config api domain

--- apps/exe-project/src/environments/environment.ts
+++ apps/exe-project/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiDomain: 'http://localhost:5000/api/v1', //'https://18.141.172.162/api/v1',
+  apiDomain: 'https://18.141.172.162/api/v1',
 };
