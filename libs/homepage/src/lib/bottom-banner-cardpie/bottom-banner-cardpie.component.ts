ad7751ddcd83986bb6cbc7fe6d0c1a6ad96304d1
hieunmtse15-772 <nguyenminhtrunghieu10@gmail.com>
2023-06-10 00:14:22 +0700

    homepage fix UI

---
+++ libs/homepage/src/lib/bottom-banner-cardpie/bottom-banner-cardpie.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'exe-project-bottom-banner-cardpie',
+  templateUrl: './bottom-banner-cardpie.component.html',
+  styleUrls: ['./bottom-banner-cardpie.component.scss'],
+})
+export class BottomBannerCardpieComponent {}
---
+++ libs/homepage/src/lib/premium-introduction/premium-introduction.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'exe-project-premium-introduction',
+  templateUrl: './premium-introduction.component.html',
+  styleUrls: ['./premium-introduction.component.scss'],
+})
+export class PremiumIntroductionComponent {}
